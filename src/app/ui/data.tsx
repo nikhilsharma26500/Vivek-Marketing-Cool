3b7a2fdc89c80629320b726fc8f9b9beba80d98f
Nikhil <nikhilsharma26500@outlook.com>
2023-10-05 10:53:06 +0530

    added data and values in navbar

---
+++ src/app/ui/data.tsx
@@ -0,0 +1,11 @@
+const data = [
+    {
+      whatsapp: '',
+      email: 'ram.babu.sharma102938@gmail.com',
+      phone1: '+91-9650888455',
+      phone2: '+91-9871968455',
+    }
+  ]
+
+  
+export default data
--- src/app/ui/navbar.tsx
+++ src/app/ui/navbar.tsx
@@ -1,8 +1,33 @@
 import React from 'react'
+import Image from 'next/image'
+import Logo from "../../../public/logo.svg"
+import Data from "./data"
 
 const navbar = () => {
   return (
-    <div>navbar</div>
+    <>
+      <div className='w-screen h-[10%] py-5 border border-black'>
+        <div className='container mx-auto flex justify-between items-center'>
+          <div>
+            <Image className='inline-block' src={Logo} width={100} height={100} alt='logo' />
+            <h1 className='inline-block'>
+              Vivek Marketing Cool
+            </h1>
+          </div>
+
+          <div>
+            {Data.map(({ whatsapp, email, phone1, phone2 }, index) => (
+              <div key={email}>
+                <p>{whatsapp}</p>
+                <p>{email}</p>
+                <p>{phone1}</p>
+                <p>{phone2}</p>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
 
